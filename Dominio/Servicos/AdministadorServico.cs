5767f75dee3a71b709572a0df6266a65185cec0c
Iara Martins Rosa <iara_martins.rosa@hotmail.com>
2025-11-09 17:40:45 -0300

    regras para autorização

---
+++ Dominio/Servicos/AdministadorServico.cs
@@ -0,0 +1,50 @@
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.DTOs;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Infraestrutura.Db;
+
+namespace MinimalApi.Dominio.Servicos;
+
+public class AdministradorServico : IAdministradorServico
+{
+    private readonly DbContexto _contexto;
+
+    public AdministradorServico(DbContexto contexto)
+    {
+        _contexto = contexto;
+    }
+    public Administrador? Login(LoginDTO loginDTO)
+    {
+        var adm = _contexto.Administradores
+            .Where(a => a.Email == loginDTO.Email && a.Senha == loginDTO.Senha)
+            .FirstOrDefault();
+        return adm;
+    }
+
+    public Administrador Incluir(Administrador administrador)
+    {
+        _contexto.Administradores.Add(administrador);
+        _contexto.SaveChanges();
+        return administrador;
+    }
+
+    public List<Administrador> Todos(int? pagina)
+    {
+        var query = _contexto.Administradores.AsQueryable();
+
+        int itensPorPagina = 10;
+
+        if (pagina != null)
+        {
+            query = query.Skip(((int)pagina - 1) * itensPorPagina).Take(itensPorPagina);
+        }
+
+        return query.ToList();
+    }
+    
+    public Administrador? BuscaPorId(int id)
+    {
+        return _contexto.Administradores.Where(a => a.Id == id).FirstOrDefault();
+    }
+}
+
